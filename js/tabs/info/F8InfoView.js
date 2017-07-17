ef44aa3a232357d1e0f26cdacb2d6ebb25092caf
trujunzhang <trujunzhang@gmail.com>
2017-07-17 19:42:09 +0800

    Android Device.

--- js/tabs/info/F8InfoView.js
+++ js/tabs/info/F8InfoView.js
@@ -35,66 +35,66 @@ const WiFiDetails = require('./WiFiDetails')
 
 
 const POLICIES_LINKS = [{
-  title: 'Terms of Service',
-  url: 'https://m.facebook.com/terms?_rdr',
+    title: 'Terms of Service',
+    url: 'https://m.facebook.com/terms?_rdr',
 }, {
-  title: 'Data Policy',
-  url: 'https://m.facebook.com/policies?_rdr',
+    title: 'Data Policy',
+    url: 'https://m.facebook.com/policies?_rdr',
 }, {
-  title: 'Code of Conduct',
-  url: 'https://www.fbf8.com/code-of-conduct',
+    title: 'Code of Conduct',
+    url: 'https://www.fbf8.com/code-of-conduct',
 }];
 
 function F8InfoView() {
-  return (
-    <ListContainer
-      title="Information"
-      backgroundImage={require('../../common/parallax/info-background.png')}
-      backgroundColor={'#47BFBF'}>
-      <InfoList />
-    </ListContainer>
-  );
+    return (
+        <ListContainer
+            title="Information"
+            backgroundImage={require('../../common/parallax/info-background.png')}
+            backgroundColor={'#47BFBF'}>
+            {/*<InfoList />*/}
+        </ListContainer>
+    )
 }
 
 function InfoList({viewer: {config, faqs, pages}, ...props}) {
-  return (
-    <PureListView
-      renderEmptyList={() => (
-        <View>
-          <WiFiDetails
-            network={config.wifiNetwork}
-            password={config.wifiPassword}
-          />
-          <CommonQuestions faqs={faqs} />
-          <LinksList title="Facebook pages" links={pages} />
-          <LinksList title="Facebook policies" links={POLICIES_LINKS} />
-        </View>
-      )}
-      {...(props: any /* flow can't guarantee the shape of props */)}
-    />
-  );
+    return (
+        <PureListView
+            renderEmptyList={() => (
+                <View>
+                    <WiFiDetails
+                        network={config.wifiNetwork}
+                        password={config.wifiPassword}
+                    />
+                    <CommonQuestions faqs={faqs}/>
+                    <LinksList title="Facebook pages" links={pages}/>
+                    <LinksList title="Facebook policies" links={POLICIES_LINKS}/>
+                </View>
+            )}
+            {...(props /* flow can't guarantee the shape of props */)}
+        />
+    );
 }
+//
+// InfoList = Relay.createContainer(InfoList, {
+//   fragments: {
+//     viewer: () => Relay.QL`
+//       fragment on User {
+//         config {
+//           wifiNetwork
+//           wifiPassword
+//         }
+//         faqs {
+//           question
+//           answer
+//         }
+//         pages {
+//           title
+//           url
+//           logo
+//         }
+//       }
+//     `,
+//   },
+// });
 
-InfoList = Relay.createContainer(InfoList, {
-  fragments: {
-    viewer: () => Relay.QL`
-      fragment on User {
-        config {
-          wifiNetwork
-          wifiPassword
-        }
-        faqs {
-          question
-          answer
-        }
-        pages {
-          title
-          url
-          logo
-        }
-      }
-    `,
-  },
-});
-
-module.exports = F8InfoView;
+module.exports = F8InfoView
