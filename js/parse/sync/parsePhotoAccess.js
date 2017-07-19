ac86377f577765fc2c129592011dd94178facc9c
trujunzhang <trujunzhang@gmail.com>
2017-07-19 21:26:14 +0800

    Android Device.

--- js/parse/sync/parsePhotoAccess.js
+++ js/parse/sync/parsePhotoAccess.js
@@ -1,11 +1,6 @@
 const Parse = require('parse/react-native')
 import moment from 'moment'
 
-const {writeParseRecord, ConfigureService, RestaurantService} = require('../realmApi').default
-
-const {getRecordsParameters} = require('../parseUtiles').default
-
-const {fromParseRecord} = require('../parseModels')
 /**
  * The states were interested in
  */
@@ -16,18 +11,6 @@ const {
 const {getLocalImagePath} = require('../fsApi')
 const RNFS = require('react-native-fs')
 
-/**
- * How to sync the data between the local and the server parse.
- *   @note: Because if the objects had been saved, it's updatedData will be changed.
- *          Using the object called record to record the updated information.
- *
- * Step1:
- *    pull the records updated are more than the last record updatedData.
- *
- * Step2:
- *   Push the records saved in the local database.
- *   @note: These records will be pull again next scheduled task.
- */
 export default class ParsePhotoAccess {
     constructor() {
     }
@@ -39,6 +22,7 @@ export default class ParsePhotoAccess {
         // const downloadDest = `${RNFS.DocumentDirectoryPath}/${((Math.random() * 1000) | 0)}.jpg`;
         const downloadDest = getLocalImagePath(id, 'orginal')
 
+        debugger
         const ret = RNFS.downloadFile({
             fromUrl: originalUrl,
             toFile: downloadDest
